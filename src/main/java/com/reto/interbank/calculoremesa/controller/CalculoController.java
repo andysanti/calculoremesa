69375b7ee467857d76886c099e60c79edec241d5
andysanti <andysanti23@gmail.com>
2024-12-18 12:39:37 -0500

    agregando LOGGER.

--- src/main/java/com/reto/interbank/calculoremesa/controller/CalculoController.java
+++ src/main/java/com/reto/interbank/calculoremesa/controller/CalculoController.java
@@ -3,6 +3,8 @@ package com.reto.interbank.calculoremesa.controller;
 import com.reto.interbank.calculoremesa.dto.InformacionCalculoDtoRequest;
 import com.reto.interbank.calculoremesa.dto.InformacionCalculoDtoResponse;
 import com.reto.interbank.calculoremesa.service.CalculoRemesaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+
 @RestController
 @RequestMapping("v1/calculoremesa/")
 public class CalculoController {
+
+    Logger logger = LoggerFactory.getLogger(CalculoController.class);
     private final CalculoRemesaService calculoRemesaService;
 
     public CalculoController(CalculoRemesaService calculoRemesaService) {
@@ -22,6 +27,7 @@ public class CalculoController {
 
     @PostMapping(produces = "application/json")
     public ResponseEntity<Mono<InformacionCalculoDtoResponse>> getCalculoRemesa(@RequestBody InformacionCalculoDtoRequest request){
+        logger.info("inicio llamada para dni {}", request.getDni());
         return new ResponseEntity<>(calculoRemesaService.calculoMoneda( request), HttpStatus.OK);
     }
 }
